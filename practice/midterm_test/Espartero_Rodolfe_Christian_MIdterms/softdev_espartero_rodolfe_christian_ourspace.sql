68ec73e5d8f89883f3dbbb04410e38a5e220f816
Rodolfe Christian T. Espartero <rodolfechristianespartero@gmail.com>
2015-03-01 12:52:25 +0800

    Inserted one record for address and insert one record for comment

--- practice/midterm_test/Espartero_Rodolfe_Christian_MIdterms/softdev_espartero_rodolfe_christian_ourspace.sql
+++ practice/midterm_test/Espartero_Rodolfe_Christian_MIdterms/softdev_espartero_rodolfe_christian_ourspace.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 01, 2015 at 04:36 AM
+-- Generation Time: Mar 01, 2015 at 05:50 AM
 -- Server version: 5.6.14
 -- PHP Version: 5.5.6
 
@@ -37,7 +37,14 @@ CREATE TABLE IF NOT EXISTS `myaddress` (
   `landline` varchar(20) DEFAULT NULL,
   `cellphone` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `myaddress`
+--
+
+INSERT INTO `myaddress` (`id`, `firstname`, `middlename`, `lastname`, `gender`, `created_at`, `home_address`, `landline`, `cellphone`) VALUES
+(1, 'Rodolfe', 'Tolete', 'Espartero', 'M', '2015-03-01 04:47:53', '808 B Von Braun St. Moonwalk Phase 1 Paranaque cit', '8320688', '09054164848');
 
 -- --------------------------------------------------------
 
@@ -53,7 +60,14 @@ CREATE TABLE IF NOT EXISTS `mycomment` (
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   KEY `mycomment_ibfk_1` (`myaddress_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `mycomment`
+--
+
+INSERT INTO `mycomment` (`id`, `myaddress_id`, `author`, `body`, `created_at`) VALUES
+(1, 1, 'Christian', 'qwepoiqweopiqwepoqwieptoqiwefslkdhfalsj,mn,bcx,kjdskjf;azncmzn,nbxcvx', '2015-03-01 04:49:32');
 
 --
 -- Constraints for dumped tables
