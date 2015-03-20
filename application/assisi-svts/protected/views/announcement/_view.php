295e9264383595e690271c8f8e790b2ac0a44edc
Jeff Almazan <jtalmazan113@gmail.com>
2015-03-20 12:37:53 -0700

    Files for Announcement table's crud

---
+++ application/assisi-svts/protected/views/announcement/_view.php
@@ -0,0 +1,21 @@
+<?php
+/* @var $this AnnouncementController */
+/* @var $data Announcement */
+?>
+
+<div class="view">
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('id')); ?>:</b>
+	<?php echo CHtml::link(CHtml::encode($data->id), array('view', 'id'=>$data->id)); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('Subject')); ?>:</b>
+	<?php echo CHtml::encode($data->Subject); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('message_box')); ?>:</b>
+	<?php echo CHtml::encode($data->message_box); ?>
+	<br />
+
+
+</div>
